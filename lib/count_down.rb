534fbfc0ab4ad65ca3b18e05ad61841ad60a39c3
Melissa F <melissa.faulner@gmail.com>
2020-10-29 19:19:19 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,7 @@
 magic_exit_number = 7
-count = 0
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!" # Work
-  count = count + 1
+count = 10
+while count > 1 do
+  puts "#{count}"
+  count -= 1
 end
+puts "Happy New Year!"
